b22c6a5e330c0faf6d7aba6239851f77e2ec6804
bardunbar <ian@dunbargames.com>
2023-10-31 19:20:51 +0000

    Partial commit to change computers

---
+++ crates/basalt_render/src/pipeline.rs
@@ -0,0 +1,21 @@
+use basalt_resource;
+
+// Render Pipeline needs the following information...
+// Bind Group Layouts
+//      Camera
+//      Texture
+//      Light*
+// Vertex Buffer Layout
+// Shader
+
+pub fn create_pipeline_from_shader(shader_path: &str, label: &str ) -> wgpu::RenderPipeline {
+
+    let shader_text = basalt_resource::load_string(shader_path).unwrap();
+    let shader_descriptor = wgpu::ShaderModuleDescriptor {
+        label: Some(label),
+        source: wgpu::ShaderSource::Wgsl(shader_text.into()),
+    };
+
+
+
+}
